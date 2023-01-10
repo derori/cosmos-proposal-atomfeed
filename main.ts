51efe367045ac78ab6fdc6d2375d9b4931b84544
derori <derori.derori@gmail.com>
2023-01-10 19:22:48 +0900

    Fix Change endpoint and types from response

--- main.ts
+++ main.ts
@@ -12,20 +12,20 @@ const fetchProposals = (async () => {
         id: "Cosmos proposals feed via cosmostaion public api.",
         copyright: "Cosmos gov",
     });
-    const url = 'https://api.cosmostation.io/v1/gov/proposals';
+    const url = 'https://api.cosmoscan.net/proposals';
     const { data } = await axios.get(url);
     let ooo: GovProposalResponse[] = data as GovProposalResponse[];
 
     ooo = ooo.sort((a, b) => {
-        return (a.proposal_id > b.proposal_id) ? -1 : 1;
+        return (a.id > b.id) ? -1 : 1;
     });
 
     for await (const oo of ooo) {
-        if (!oo.proposal_id) continue;
+        if (!oo.id) continue;
         feed.addItem({
             title: `VotingEnd: ${moment(oo.voting_end_time).toISOString()} **${oo.title}`,
-            link: `https://www.mintscan.io/cosmos/proposals/${oo.proposal_id}`,
-            date: moment(oo.submit_time).toDate(),
+            link: `https://www.mintscan.io/cosmos/proposals/${oo.id}`,
+            date: moment(Number(oo.submit_time) * 1000).toDate(),
             id: oo.tx_hash,
         });
 
@@ -35,18 +35,27 @@ const fetchProposals = (async () => {
     //    console.dir(ooo);
 
     interface GovProposalResponse {
-        proposal_id: string,
+        id: string,
         tx_hash: string,
+        type: string,
         proposer: string,
-        moniker: string,
+        proposer_address: string,
         title: string,
         description: string,
-        proposal_type: string,
-        proposal_status: string,
-        submit_time: string,
-        deposit_end_time: string,
-        voting_start_time: string,
-        voting_end_time: string,
+        status: string,
+        votes_yes: string,
+        votes_abstain: string,
+        votes_no: string,
+        votes_no_with_veto: string,
+        submit_time: Date,
+        deposit_end_time: Date,
+        total_deposits: string,
+        voting_start_time: Date,
+        voting_end_time: Date,
+        voters: number,
+        participation_rate: string,
+        turnout: string,
+        activity: boolean
     }
 });
 const server = http.createServer(async (req: IncomingMessage, res: ServerResponse) => {
